e03716c719b950b34e6f46c0d74d69fb9d69bb72
Pradeep S <117376703+Pradeep2535@users.noreply.github.com>
2024-07-29 11:48:55 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,6 +1,6 @@
 const foodItems = [
-    { name: 'Frankie', price: 100.00, count: 1, web: "https://th.bing.com/th/id/OIP.x-1Rn2BPDZ8QNjTmUbx3QAHaHa?pid=ImgDet&w=200&h=200&c=7&dpr=1.3" },
-    { name: 'Chappatti', price: 100.00, count: 1, web: "https://th.bing.com/th/id/OIP.2QoIkJxfhNslsJrY8k2TfAHaFj?w=235&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" },
+    { name: 'Ramen bowl', price: 100.00, count: 1, web: "https://th.bing.com/th/id/OIP.x-1Rn2BPDZ8QNjTmUbx3QAHaHa?pid=ImgDet&w=200&h=200&c=7&dpr=1.3" },
+    { name: 'Chapati', price: 100.00, count: 1, web: "https://th.bing.com/th/id/OIP.2QoIkJxfhNslsJrY8k2TfAHaFj?w=235&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" },
 
 ];
 
